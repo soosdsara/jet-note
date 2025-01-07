7c8a732031e42240e8d9da76db82b9c1eb158462
Soós Sára <soosdsara27@gmail.com>
2025-01-07 12:56:11 +0100

    Setup the Project and the structure

---
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.jetnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnote.ui.theme.JetNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetNoteTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetNoteTheme {
+    }
+}
